a43d76375758499ee714e457ac14817ab1add626
hannahpae <hannahpae7@gmail.com>
2021-05-19 15:35:34 -0700

    created scripts

---
+++ scripts/scalePlot.R
@@ -0,0 +1,11 @@
+#plot 1
+play_by_play <- read.csv("./data/NFLPlaybyPlay20092018.csv")
+
+library("dplyr")
+library("ggplot2")
+library("leaflet")
+library("plotly")
+
+
+##you must describe why you included the chart (e.g., what it attempts to seeks to express), 
+##and what information it reveals
