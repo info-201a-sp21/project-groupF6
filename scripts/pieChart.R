5472cb76c695fb1baccbd402c3b9a1a3e67897de
hannahpae <hannahpae7@gmail.com>
2021-05-19 16:00:44 -0700

    added another chart file

---
+++ scripts/pieChart.R
@@ -0,0 +1,13 @@
+#plot 2
+play_by_play <- read.csv("./data/NFLPlaybyPlay20092018.csv", stringsAsFactors = FALSE))
+
+library("dplyr")
+library("ggplot2")
+library("leaflet")
+library("plotly")
+
+##side of field vs postteam winning probabilty 
+
+
+##you must describe why you included the chart (e.g., what it attempts to seeks to express), 
+##and what information it reveals
--- scripts/scalePlot.R
+++ scripts/scalePlot.R
@@ -1,10 +1,17 @@
 #plot 1
-play_by_play <- read.csv("./data/NFLPlaybyPlay20092018.csv")
+play_by_play <- read.csv("./data/NFLPlaybyPlay20092018.csv", stringsAsFactors = FALSE)
 
 library("dplyr")
 library("ggplot2")
 library("leaflet")
 library("plotly")
+
+#yardline 100 vs defeated team
+
+
+plot <- ggplot(data = play_by_play) +
+  geom_(mapping = aes(x = num_killed, y = num_injured, color = state)) +
+  ggtitle("Deaths and Injuries due to Shootings")
 
 
 ##you must describe why you included the chart (e.g., what it attempts to seeks to express), 
