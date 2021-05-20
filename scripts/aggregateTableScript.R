b47a064bdc1a0fe1c6b12f48997008188660d029
Ryan Tran <ryantamtran@gmail.com>
2021-05-19 23:23:02 -0700

    grouped by one column

--- scripts/aggregateTableScript.R
+++ scripts/aggregateTableScript.R
@@ -1,5 +1,3 @@
-library("dplyr")
-
 athlete_events <- read.csv("data/athlete_events.csv", stringsAsFactors = FALSE)
 
 # This table is group by Team and it includes the teams who have at least won 
@@ -11,21 +9,7 @@ aggregate_info <- function(df) {
     group_by(Team) %>%
     filter(Medal != "NA") %>%
     mutate(Total_Medals = 1) %>%
-<<<<<<< HEAD
-    summarize(Total_Medals = sum(Total_Medals, na.rm = TRUE, .groups = "keep")) %>%
-    arrange(-Total_Medals)
-}
-test <- function(df) {
-  df %>%
-  group_by(Team, NOC, Medal) %>%
-    filter(Medal != "NA") %>%
-    mutate(Total_Medals = 1) %>%
-    summarise(Total_Medals = sum(Total_Medals, na.rm = TRUE), Team, NOC,  Medal) %>%
-    arrange(-Total_Medals)
-=======
     summarize(Total_Medals = sum(Total_Medals, na.rm = TRUE)) %>%
     arrange(-Total_Medals) %>% 
     head(10)
->>>>>>> c2ece46ccee089c5ed9bc54f0335b22566bc0985
 }
-
