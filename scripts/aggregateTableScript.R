cb1ed74705dcdb4c7edbdf613cdd9b4094ae843b
igz10 <garibayisaac3@gmail.com>
2021-05-19 22:59:56 -0700

    grouped by one column

--- scripts/aggregateTableScript.R
+++ scripts/aggregateTableScript.R
@@ -2,11 +2,17 @@ library("dplyr")
 
 athlete_events <- read.csv("data/athlete_events.csv", stringsAsFactors = FALSE)
 
+# This table is group by Team and it includes the teams who have at least won 
+# one medal. The total medal counts the teams total medals whether it be Gold,
+# Silver, or Bronze in order from most to least. 
+
 aggregate_info <- function(df) {
   df %>%
-    group_by(Team, NOC, Medal) %>%
+    group_by(Team) %>%
     filter(Medal != "NA") %>%
     mutate(Total_Medals = 1) %>%
     summarize(Total_Medals = sum(Total_Medals, na.rm = TRUE)) %>%
-    arrange(-Total_Medals)
+    arrange(-Total_Medals) %>% 
+    head(10)
 }
+
