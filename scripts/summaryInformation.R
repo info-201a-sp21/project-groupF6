b4d26feb5aa78e6e05eba9386bac05a6a87eab38
Veronica Mendoza <veronhm@uw.edu>
2021-05-19 22:50:10 -0700

    made changes to summary

--- scripts/summaryInformation.R
+++ scripts/summaryInformation.R
@@ -1,16 +1,25 @@
+library(dplyr)
+athlete_events <- read.csv("data/athlete_events.csv", stringsAsFactors = FALSE)
 
 get_summary_info <- function(dataset) {
   summary <- list()
   summary$length <- length(dataset)
-  summary$events <- unique(dataset$Sport)
-  summary$teams <- unique(dataset$NOC)
+  summary$num_events <- length(unique(dataset$Sport))
+  summary$num_teams <- length(unique(dataset$NOC))
   summary$gold_winners <- dataset %>%
     filter(Medal == "Gold") %>%
-    pull(NOC)
+    mutate(freq = 1) %>%
+    group_by(NOC) %>%
+    summarize(gold_medals = sum(freq, na.rm = FALSE)) %>%
+    arrange(desc(gold_medals)) %>%
+    top_n(5)
   summary$oldest_data <- min(dataset$Year, na.rm = FALSE)
   summary$newest_data <- max(dataset$Year, na.rm = FALSE)
   return(summary)
 }
+
+get_summary_info(athlete_events)
+
 
 
 
